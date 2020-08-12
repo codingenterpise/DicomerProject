7c59802ac127e001cfe9990f0bb0aae02dcd3560
Hector Espinoza <codingenterprise@icloud.com>
2020-08-12 12:14:41 -0700

    errorfolder

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\anoni\OneDrive\Escritorio\flutter_login_ui-master"
+export "FLUTTER_APPLICATION_PATH=C:\Users\anoni\OneDrive\Escritorio\gitclone\DicomerProject"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
